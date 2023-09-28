5d81dc05d4709e6200d5497c747a9d11d4448ed6
MantonySLX <Milton.Antony@SalesLabX.com>
2023-09-28 14:25:15 -0500

    formating

--- api/countdown.js
+++ api/countdown.js
@@ -4,18 +4,18 @@ const { parseISO, differenceInSeconds } = require('date-fns');
 
 module.exports = async (req, res) => {
   try {
-    console.log("Serverless function invoked");  // Debug log
-
-    // Get end_date from query parameters
+    // Get end_date and styling parameters from query parameters
     const endDateStr = req.query.end_date || '';
-    console.log("Received end_date:", endDateStr);  // Debug log
+    const fontSize = req.query.fontSize || '24';
+    const fontColor = req.query.fontColor || '#000000';
+    const font = req.query.font || 'Arial';
+    const bgColor = req.query.bgColor || '#FFFFFF';
+    const labelColor = req.query.labelColor || '#000000';
+    const showLabels = req.query.showLabels === 'true';
 
     const endDate = parseISO(endDateStr);
     const now = new Date();
-
-    // Calculate time left in seconds
     const timeLeft = differenceInSeconds(endDate, now);
-    console.log("Calculated time left:", timeLeft);  // Debug log
 
     // Calculate days, hours, minutes
     const days = Math.floor(timeLeft / 86400);
@@ -26,25 +26,21 @@ module.exports = async (req, res) => {
     const canvas = createCanvas(300, 100);
     const ctx = canvas.getContext('2d');
 
-    // Draw background
-    ctx.fillStyle = '#FFFFFF';
+    // Apply background color
+    ctx.fillStyle = bgColor;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // Draw text
-    ctx.fillStyle = '#000000';
-    ctx.font = '24px Arial';
-    ctx.fillText(`${days} Days ${hours} Hours ${minutes} Minutes`, 10, 50);
+    // Apply font and draw text
+    ctx.font = `${fontSize}px ${font}`;
+    ctx.fillStyle = fontColor;
+    const text = showLabels ? `${days} Days ${hours} Hours ${minutes} Minutes` : `${days}:${hours}:${minutes}`;
+    ctx.fillText(text, 10, 50);
 
     // Set response type to PNG
     res.setHeader('Content-Type', 'image/png');
-
-    // Send the image
     const buffer = canvas.toBuffer('image/png');
     res.send(buffer);
-
-    console.log("Image sent successfully");  // Debug log
   } catch (error) {
-    console.error("Error occurred:", error);  // Debug log
     res.status(500).send('Internal Server Error');
   }
 };
