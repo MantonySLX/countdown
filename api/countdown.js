d8f2871618ff81bbe63bbc65df8cf2af67810b01
MantonySLX <Milton.Antony@SalesLabX.com>
2023-09-28 18:54:15 -0500

    Update countdown.js

--- api/countdown.js
+++ api/countdown.js
@@ -1,55 +1,66 @@
 
-const { createCanvas, loadImage } = require('@napi-rs/canvas');
-const { parseISO, differenceInSeconds } = require('date-fns');
+const { createCanvas, loadImage } = require("@napi-rs/canvas");
+const GIFEncoder = require("gifencoder");
+const { parseISO, differenceInSeconds } = require("date-fns");
 
 module.exports = async (req, res) => {
   try {
-    // Get end_date from query parameters
-    const endDateStr = req.query.end_date || '';
+    const endDateStr = req.query.end_date || "";
     const endDate = parseISO(endDateStr);
     const now = new Date();
     const timeLeft = differenceInSeconds(endDate, now);
 
-    // Calculate days, hours, minutes, seconds
+    // Calculate days, hours, minutes
     const days = Math.floor(timeLeft / 86400);
     const hours = Math.floor((timeLeft % 86400) / 3600);
     const minutes = Math.floor((timeLeft % 3600) / 60);
-    const seconds = Math.floor(timeLeft % 60);
 
-    // Create canvas and context
     const canvas = createCanvas(300, 200);
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext("2d");
+    const encoder = new GIFEncoder(300, 200);
 
-    // Apply background color
-    ctx.fillStyle = '#ffd54f';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    encoder.createReadStream().pipe(res);
 
-    // Draw numbers and labels individually with controlled x, y coordinates
-    const unitValues = [days, hours, minutes, seconds];
-    const unitLabels = ['Days', 'Hours', 'Minutes', 'Seconds'];
-    let xOffset = 10;
+    encoder.start();
+    encoder.setRepeat(0);
+    encoder.setDelay(1000); // 1 second
+    encoder.setQuality(10);
 
-    unitValues.forEach((value, index) => {
-      // Draw numbers
-      ctx.font = '24px Arial';
-      ctx.fillStyle = '#333';
-      ctx.fillText(value.toString(), xOffset, 50);
+    const unitValues = [days, hours, minutes];
+    const unitLabels = ["Days", "Hours", "Minutes"];
 
-      // Draw labels
-      ctx.fillStyle = '#333';
-      ctx.font = '14px Arial';
-      ctx.fillText(unitLabels[index], xOffset, 50 + 30);  // Position labels below the numbers
+    for (let i = 57; i <= 60; i++) {
+      ctx.fillStyle = "#ffd54f";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-      // Calculate the width of the text to properly space out the next unit
-      const textWidth = ctx.measureText(value.toString()).width;
-      xOffset += textWidth + 40;
-    });
+      let xOffset = 10;
 
-    // Set response type to PNG
-    res.setHeader('Content-Type', 'image/png');
-    const buffer = canvas.toBuffer('image/png');
-    res.send(buffer);
+      unitValues.forEach((value, index) => {
+        ctx.font = "24px Arial";
+        ctx.fillStyle = "#333";
+        ctx.fillText(value.toString(), xOffset, 50);
+
+        ctx.fillStyle = "#333";
+        ctx.font = "14px Arial";
+        ctx.fillText(unitLabels[index], xOffset, 80);
+
+        const textWidth = ctx.measureText(value.toString()).width;
+        xOffset += textWidth + 40;
+      });
+
+      ctx.font = "24px Arial";
+      ctx.fillStyle = "#333";
+      ctx.fillText(i.toString(), xOffset, 50);
+
+      ctx.fillStyle = "#333";
+      ctx.font = "14px Arial";
+      ctx.fillText("Seconds", xOffset, 80);
+
+      encoder.addFrame(ctx);
+    }
+
+    encoder.finish();
   } catch (error) {
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 };
